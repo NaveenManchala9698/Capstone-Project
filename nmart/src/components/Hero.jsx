4e02d3a5579b2ab26790cb0de0bacba9a6eba02f
Naveen Kumar Manchala <100999011+NaveenManchala9698@users.noreply.github.com>
2022-07-27 13:53:53 +0200

    details page

--- nmart/src/components/Hero.jsx
+++ nmart/src/components/Hero.jsx
@@ -3,12 +3,12 @@ import Card from "react-bootstrap/Card";
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div className="hero mt-5">
       <Card className="bg-dark text-white border-0">
         <Card.Img
           src="https://images.pexels.com/photos/845434/pexels-photo-845434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="Card image"
-          height="650px"
+          height="700px"
         />
         <Card.ImgOverlay className="d-flex flex-column justify-content-center">
           <div className="container">
--- nmart/src/pages/ProductDetail.jsx
+++ nmart/src/pages/ProductDetail.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { useEffect } from "react";
 import { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import Navbar from "../components/Navbar";
 import porductsJson from "../data/products.json";
+import "../css/ProductDetails.css";
+import { Cart3, CircleFill, Heart } from "react-bootstrap-icons";
 
 const ProductDetail = () => {
   const [productDetails, setProductDetails] = useState([]);
@@ -29,7 +33,8 @@ const ProductDetail = () => {
       if (response.ok) {
         const data = await response.json();
         console.log(data);
-        setProductDetails(data);
+        const final = data.product;
+        setProductDetails(final);
       } else {
         console.log("Error!!");
       }
@@ -38,34 +43,143 @@ const ProductDetail = () => {
     }
   };
   return (
-    <div>
-      <Container>
-        <Row>
-          {productDetails && (
-            <>
-              <Col md={6}>
-                <img
-                  src={
-                    porductsJson.results[0].defaultArticle.normalPicture[0].url
-                  }
-                  alt={porductsJson.results[0].name}
-                  height="600px"
-                  width="400px"
-                />
-              </Col>
-              <Col md={6}>
-                <h1>{productDetails.product.name}</h1>
-                <h5>
-                  <b>Brand Name: </b>
-                  {porductsJson.results[0].brandName}
-                </h5>
-                <p>{productDetails.product.description}</p>
-              </Col>
-            </>
-          )}
-        </Row>
-      </Container>
-    </div>
+    <>
+      <Navbar />
+      <div style={{ marginTop: "6rem", backgroundColor: "lightgray" }}>
+        <Container>
+          <Row>
+            {productDetails && (
+              <>
+                <Col md={6} className="mt-5 ">
+                  <img
+                    src={
+                      porductsJson.results[0].defaultArticle.normalPicture[0]
+                        .url
+                    }
+                    alt={porductsJson.results[0].name}
+                    height="600px"
+                    width="400px"
+                  />
+                </Col>
+                <Col md={6} className="mt-5 text-left">
+                  <h1>
+                    <b>{porductsJson.results[0].brandName}</b>
+                  </h1>
+                  <div className="d-flex">
+                    <h4>{productDetails.name}</h4>
+                    <h4 className="text-warning ml-2">
+                      <b>(New Arrival)</b>
+                    </h4>
+                  </div>
+                  <hr />
+
+                  <div className="d-flex">
+                    <h3 className="mr-3">
+                      <b>Price: </b>
+                      {porductsJson.results[0].price.formattedValue}
+                    </h3>
+                    <a href="#" className="cart_btn mx-1">
+                      <Cart3 className="mx-2" />
+                      ADD TO CART
+                    </a>
+                    <a href="#" class="wishlist_btn mx-1">
+                      <Heart className="mx-2" />
+                      WISHLIST
+                    </a>
+                  </div>
+                  <div className="mt-3">
+                    <div class="dropdown">
+                      <button
+                        class="btn btn-primary dropdown-toggle"
+                        type="button"
+                        id="dropdownMenuButton"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        More Colors
+                      </button>
+                      <div
+                        class="dropdown-menu"
+                        aria-labelledby="dropdownMenuButton"
+                      >
+                        {productDetails.articlesList &&
+                          productDetails.articlesList.map((eachColor) => (
+                            <li className="px-3 py-1">
+                              <span className="mr-2">
+                                <CircleFill
+                                  style={{
+                                    color: `${eachColor.color.rgbColor}`,
+                                  }}
+                                />
+                              </span>
+                              {eachColor.color.text}
+                            </li>
+                          ))}
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mt-3">
+                    <h5>
+                      <b>SELECT SIZE: </b>
+                    </h5>
+                    <a href="#" class="size-buttons">
+                      S
+                    </a>
+                    <a href="#" class="size-buttons">
+                      M
+                    </a>
+                    <a href="#" class="size-buttons">
+                      L
+                    </a>
+                    <a href="#" class="size-buttons">
+                      XL
+                    </a>
+                  </div>
+                  <div className="mt-3">
+                    <li>100% Original Products</li>{" "}
+                    <li>Pay on delivery available</li>
+                    <li>Easy 30 days returns and exchanges</li>
+                    <li>Try & Buy available</li>
+                  </div>
+                  <hr />
+
+                  <div className="mt-3">
+                    <h5>
+                      <b>PRODUCT DETAILS: </b>
+                    </h5>
+                    <p>{productDetails.description}</p>
+                  </div>
+                  <div className="mt-3">
+                    <h5>
+                      <b>SIZE & FIT: </b>
+                    </h5>
+                    <p>The model(height 5'5) is wearing size M.</p>
+                  </div>
+                  <div className="mt-3">
+                    <>
+                      <h5>
+                        <b>MATERIAL: </b>
+                      </h5>
+                      {productDetails.materialDetails &&
+                        productDetails.materialDetails.map((eachMaterial) => (
+                          <li className="px-3 py-1">{eachMaterial.name}</li>
+                        ))}
+                    </>
+                  </div>
+                  <div className="mt-3">
+                    <>
+                      <h5>SPECIFICATIONS:</h5>
+                    </>
+                  </div>
+                </Col>
+              </>
+            )}
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 };
 
